96a2ff7a252a1680c1101d31d479dd6c98adb752
VietDaoQuoc <daoquocviet1610@gmail.com>
2025-08-24 11:39:40 +0200

    add unet.py and modify data.yml

---
+++ src/models/unet.py
@@ -0,0 +1,91 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+## from: https://github.com/clemkoa/u-net/blob/master/unet/unet.py
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(DoubleConv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_ch, out_ch, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class Up(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(Up, self).__init__()
+        self.up_scale = nn.ConvTranspose2d(in_ch, out_ch, kernel_size=2, stride=2)
+
+    def forward(self, x1, x2):
+        x2 = self.up_scale(x2)
+
+        diffY = x1.size()[2] - x2.size()[2]
+        diffX = x1.size()[3] - x2.size()[3]
+
+        x2 = F.pad(x2, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+        return torch.cat([x2, x1], dim=1)
+
+
+class DownLayer(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(DownLayer, self).__init__()
+        self.pool = nn.MaxPool2d(2)
+        self.conv = DoubleConv(in_ch, out_ch)
+
+    def forward(self, x):
+        return self.conv(self.pool(x))
+
+
+class UpLayer(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(UpLayer, self).__init__()
+        self.up = Up(in_ch, out_ch)
+        self.conv = DoubleConv(in_ch, out_ch)
+
+    def forward(self, x1, x2):
+        a = self.up(x1, x2)
+        return self.conv(a)
+
+
+class UNet(nn.Module):
+    def __init__(self, in_ch=4, out_ch=2):
+        
+        super(UNet, self).__init__()
+        self.conv1 = DoubleConv(in_ch, 64)
+        self.down1 = DownLayer(64, 128)
+        self.down2 = DownLayer(128, 256)
+        self.down3 = DownLayer(256, 512)
+        self.down4 = DownLayer(512, 1024)
+
+        self.up1 = UpLayer(1024, 512)
+        self.up2 = UpLayer(512, 256)
+        self.up3 = UpLayer(256, 128)
+        self.up4 = UpLayer(128, 64)
+
+        self.last_conv = nn.Conv2d(64, out_ch, kernel_size=1)
+
+    def forward(self, x):
+        x1 = self.conv1(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+
+        x1_up = self.up1(x4, x5)
+        x2_up = self.up2(x3, x1_up)
+        x3_up = self.up3(x2, x2_up)
+        x4_up = self.up4(x1, x3_up)
+
+        return self.last_conv(x4_up)
+
+
