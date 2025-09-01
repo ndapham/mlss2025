58c8537f6b0c9186ab489afe2c2d7462ac38785c
Nguyen Duc Anh Pham <pducanh2000@gmail.com>
2025-09-01 18:03:16 +0200

    Wrap up

---
+++ src/models/unet_2.py
@@ -0,0 +1,106 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+
+class SEBlock(nn.Module):
+    def __init__(self, channels, reduction=16):
+        super(SEBlock, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(channels, channels // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channels // reduction, channels, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y.expand_as(x)
+
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_ch, out_ch, use_se=True):
+        super(DoubleConv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_ch, out_ch, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+        )
+        self.se = SEBlock(out_ch) if use_se else nn.Identity()
+
+    def forward(self, x):
+        return self.se(self.conv(x))
+
+
+class Up(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(Up, self).__init__()
+        self.up_scale = nn.ConvTranspose2d(in_ch, out_ch, kernel_size=2, stride=2)
+
+    def forward(self, x1, x2):
+        x1 = self.up_scale(x1)
+
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+        return torch.cat([x2, x1], dim=1)
+
+
+class DownLayer(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(DownLayer, self).__init__()
+        self.pool = nn.MaxPool2d(2)
+        self.conv = DoubleConv(in_ch, out_ch)
+
+    def forward(self, x):
+        return self.conv(self.pool(x))
+
+
+class UpLayer(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(UpLayer, self).__init__()
+        self.up = Up(in_ch, out_ch)
+        self.conv = DoubleConv(in_ch, out_ch)
+
+    def forward(self, x1, x2):
+        a = self.up(x1, x2)
+        return self.conv(a)
+
+
+class UNet2(nn.Module):
+    def __init__(self, in_ch=4, out_ch=1):
+        super(UNet2, self).__init__()
+        self.conv1 = DoubleConv(in_ch, 64)
+        self.down1 = DownLayer(64, 128)
+        self.down2 = DownLayer(128, 256)
+        self.down3 = DownLayer(256, 512)
+        self.down4 = DownLayer(512, 1024)
+
+        self.up1 = UpLayer(1024, 512)
+        self.up2 = UpLayer(512, 256)
+        self.up3 = UpLayer(256, 128)
+        self.up4 = UpLayer(128, 64)
+
+        self.last_conv = nn.Conv2d(64, out_ch, kernel_size=1)
+
+    def forward(self, x):
+        x1 = self.conv1(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+
+        u1 = self.up1(x5, x4)
+        u2 = self.up2(u1, x3)
+        u3 = self.up3(u2, x2)
+        u4 = self.up4(u3, x1)
+
+        return self.last_conv(u4)
--- src/tools/predict.py
+++ src/tools/predict.py
@@ -1,7 +1,91 @@
-import
+import os
+import sys
+import matplotlib.pyplot as plt
+from pydantic import config
+import torch 
+from torch.utils.data import DataLoader
+from torchsummary import summary
+from tqdm import tqdm
+import wandb
+import argparse
+
+root = os.path.abspath(os.path.join(os.path.dirname("."), '../..'))
+sys.path.insert(0, root)
+
+from src.utils.io import load_dataset, read_yaml_file
+from src.utils.util import *
+from src.models.unet_2 import UNet2
+from src.models.unet import UNet
+from src.models.res_unet import ResUNet
+from src.models.res_attn_unet import ResAttentionUNet
+from src.loader.datasets import ScribbleDataset 
+from src.criterion.metric_based import DiceBCELoss
+from src.models.dino import *
+
+if __name__ == "__main__":
+    checkpoint_path = "/Users/pducanh/Desktop/mlss2025/data/output/checkpoints/unet_100_88_0.5952545125453903.pt"
+    file_name = checkpoint_path.split("/")[-1]
+    print(file_name)
+
+    model_name = file_name.split("_")[0]
+
+    prediction_folder= "/Users/pducanh/Desktop/mlss2025/data/output/predictions"
+    output_folder_path = os.path.join(prediction_folder, file_name)
+    if not os.path.exists(output_folder_path):
+        os.makedirs(output_folder_path)
+
+    if model_name == "unet":
+        model = UNet()
+    elif model_name == "resunet":
+        model = ResUNet()
+    elif model_name == "unet2":
+        model = UNet2()
+    else: 
+        model = ResAttentionUNet()
+
+    model.load_state_dict(torch.load(checkpoint_path, map_location= "cpu"))
+    print("Model loaded ...")
+
+    test_images_folder = "/Users/pducanh/Desktop/mlss2025/data/input/test1/images"
+    test_scribbles_folder = "/Users/pducanh/Desktop/mlss2025/data/input/test1/scribbles"
+    test_ground_truth_folder = "/Users/pducanh/Desktop/mlss2025/data/input/test1/ground_truth"
+
+    images_test, scrib_test, gt_test, fnames_test, palette = load_dataset(
+       test_images_folder, test_scribbles_folder, test_ground_truth_folder
+    )
+    device = "cpu"
+
+    test_dataset = ScribbleDataset(images_test, scrib_test, fnames_test, gt_test, augment_rate=0)
+    test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
+
+    preds = []
+    file_names = []
+
+    for i, batch_infor in tqdm(enumerate(test_loader)):
+        batch_images, batch_scribbles, batch_annos, batch_fnames = batch_infor
+        B, C, H, W = batch_images.shape
+        batch_images = batch_images.to(device)
+        batch_scribbles = batch_scribbles.to(device)
+        batch_annos = batch_annos.to(device)
+        file_names.extend(batch_fnames)
+
+        batch_input = torch.cat([batch_images, batch_scribbles], dim=1)
+        batch_logits = model(batch_input)
+
+        
+        batch_preds = (batch_logits > 0).to(batch_logits.dtype) # (B , 1, H, W)
+        preds.append(batch_preds)
+
+    total_pred = torch.cat(preds, dim=0).detach().cpu().numpy()
+    for i in range(total_pred.shape[0]):
+        image_tensor = total_pred[i]
+        image_tensor = image_tensor.view(1, H, W)
+        convert_to_pil_image = transforms.ToPILImage()
+        pil_image = convert_to_pil_image(image_tensor)
+        image_file_name = file_names[i]
+        image_file_path = os.path.join(output_folder_path, image_file_name)
+        pil_image.save(image_file_path)
 
 
-def predict(cfg, model, test_images, test_scribbles):
-    model.eval()
-    model_name = model.__class__.__name__
-
+        
+        
